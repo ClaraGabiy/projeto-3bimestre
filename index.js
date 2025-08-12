33dd43caab2d04ee3b1e0b81fac54b1431705234
clara <clara>
2025-08-12 12:02:30 -0300

    rota post

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ const PORT = 3000
 
 app.use(express.json())
 
+let usuarios = []
+let contador = 1
+
 //ROTA DE TESTE
 app.get("/status", (req, res) => {
   res.json({message: "API Online"})
+})
+
+app.post("/usuarios", (req, res) => {
+  const { nome } = req.body
+
+  const novoUsuario = {
+    id: contador++,
+    nome
+  }
+
+  usuarios.push(novoUsuario)
+  res.status(201).json(novoUsuario)
 })
 
 app.listen(PORT, () => {
